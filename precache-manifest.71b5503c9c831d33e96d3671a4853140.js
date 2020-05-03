6971ca4c5b18abd61bc4e66468d06378bd8fc5b2
piruthuvi22 <piruthuviraj22@gmail.com>
2020-05-03 21:49:44 +0530

    Updates

---
+++ precache-manifest.71b5503c9c831d33e96d3671a4853140.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12c48de1770ac441d4a82fbdad505aa6",
+    "url": "/tic-tac-toe-by-piruthuvi/index.html"
+  },
+  {
+    "revision": "2ca8a047b8c047e6b034",
+    "url": "/tic-tac-toe-by-piruthuvi/static/css/main.01d73037.chunk.css"
+  },
+  {
+    "revision": "c6c6eeaa5db8af37d778",
+    "url": "/tic-tac-toe-by-piruthuvi/static/js/2.3549a394.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe-by-piruthuvi/static/js/2.3549a394.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ca8a047b8c047e6b034",
+    "url": "/tic-tac-toe-by-piruthuvi/static/js/main.e00d3385.chunk.js"
+  },
+  {
+    "revision": "c272ae653992327e4f2d",
+    "url": "/tic-tac-toe-by-piruthuvi/static/js/runtime-main.ebdacbf5.js"
+  },
+  {
+    "revision": "e00229b840d36b6390ebd7630cb812a4",
+    "url": "/tic-tac-toe-by-piruthuvi/static/media/corona.e00229b8.gif"
+  },
+  {
+    "revision": "fb69b0b94fd3e40bf041be3db4b8b5e9",
+    "url": "/tic-tac-toe-by-piruthuvi/static/media/human.fb69b0b9.gif"
+  },
+  {
+    "revision": "a92faed704289cb91a45bfb7a4a1e4d7",
+    "url": "/tic-tac-toe-by-piruthuvi/static/media/music.a92faed7.mp3"
+  },
+  {
+    "revision": "cb965701af591a2147d9b5e800dd126d",
+    "url": "/tic-tac-toe-by-piruthuvi/static/media/pop1.cb965701.mp3"
+  },
+  {
+    "revision": "f4051d60c5ec6351ac9a6829fcbf85f7",
+    "url": "/tic-tac-toe-by-piruthuvi/static/media/pop2.f4051d60.mp3"
+  }
+]);
